8e70bcb7eee996e3c7df637d2d2395adb40d1807
sztojkovics <53936996+sztojkovics@users.noreply.github.com>
2025-11-03 17:05:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ st.markdown("Tartsd számon, hogy melyik napon mi volt a nap szava – és ki k�
 st.header("🔍 Keresés szóra")
 szo = st.text_input("Adj meg egy szót:")
 if szo:
-    df["szó_normalizalt"] = df["szó"].apply(remove_accents)
+    df["szó_normalizalt"] = df["szó"]..astype(str).apply(remove_accents)
     szo_norm = remove_accents(szo)
-    talalatok = df[df["szó_normalizalt"].str.contains(szo_norm)]
+    talalatok = df[df["szó_normalizalt"].str.contains(szo_norm, na=False)]
     if talalatok.empty:
         st.info(f"❌ A '{szo}' szó még nem szerepelt.")
     else:
