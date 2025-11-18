b5b77d6e0f47baa925087e77b137a16b64b73340
sztojkovics <53936996+sztojkovics@users.noreply.github.com>
2025-11-18 14:13:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def remove_accents(text):
 @st.cache_data
 def load_data():
     try:
-        return pd.read_csv(FILENAME, parse_dates=["dátum"]).dropna()
+        return pd.read_csv(FILENAME, parse_dates=["dátum"]).dropna().sort_values("dátum")
     except FileNotFoundError:
         return pd.DataFrame(columns=["dátum", "szó", "beküldő"])
 
